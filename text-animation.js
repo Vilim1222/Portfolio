857a36e1b47f351e03b9694e6b6c7740c46f9e44
Vilim1222 <vilim.tavcar@gmail.com>
2025-02-19 11:28:19 +0100

    Update text-animation.js

--- text-animation.js
+++ text-animation.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+/*document.addEventListener("DOMContentLoaded", function () {
     const textElement = document.getElementById("animated-text");
 
     const texts = [
